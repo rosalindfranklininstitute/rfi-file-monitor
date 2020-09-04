4224b30160ff8b28f8a850b129ce6406fd8bad4b
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-09-04 17:09:12 +0100

    Fix LGTM warning

--- rfi_file_monitor/utils/widgetparams.py
+++ rfi_file_monitor/utils/widgetparams.py
@@ -1,6 +1,6 @@
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gio, GLib, Gtk
+from gi.repository import Gtk
 from pathlib import Path
 from typing import Final, Any, List, final
 

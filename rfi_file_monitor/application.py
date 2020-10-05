1fbb9a29b63ed59ef22e7d5b9d0278dc79cb47d9
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-10-05 17:11:49 +0100

    Bump version: 0.1.9 → 0.1.10

--- rfi_file_monitor/application.py
+++ rfi_file_monitor/application.py
@@ -155,7 +155,7 @@ class Application(Gtk.Application):
             modal=True,
             authors=["Tom Schoonjans"],
             logo=logo,
-            version="0.1.9",
+            version="0.1.10",
             )
         about_dialog.present()
 

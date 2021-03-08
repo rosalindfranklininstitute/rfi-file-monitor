2529ecbfb38e6393cde8b2a4d090f01fa4dc42c2
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-08 13:47:51 +0000

    S3Uploader: make transfer cancellable

--- rfi_file_monitor/utils/s3.py
+++ rfi_file_monitor/utils/s3.py
@@ -1,14 +1,21 @@
+from __future__ import annotations
+
 import boto3.s3.transfer
 
 from ..file import File
+from . import ExitableThread
 
 import os
 import hashlib
 from typing import Optional, Union
+import threading
+import logging
+
+logger = logging.getLogger(__name__)
 
 KB = 1024
 MB = KB * KB
-TransferConfig = boto3.s3.transfer.TransferConfig(max_concurrency=1, multipart_chunksize=8*MB, multipart_threshold=8*MB)
+TransferConfig = boto3.s3.transfer.TransferConfig(max_concurrency=1, multipart_chunksize=8*MB, multipart_threshold=8*MB, use_threads=False)
 
 def calculate_etag(file: Union[str, os.PathLike]):
     # taken from https://stackoverflow.com/a/52300584
@@ -37,7 +44,14 @@ def calculate_etag(file: Union[str, os.PathLike]):
 # taken from https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
 class S3ProgressPercentage(object):
 
-    def __init__(self, file: File, filename: Union[str, os.PathLike], operation_index: int, size: Optional[float] = None):
+    def __init__(self,
+        file: File,
+        filename: Union[str, os.PathLike],
+        operation_index: int,
+        size: Optional[float] = None,
+        manager: Optional[boto3.s3.transfer.TransferManager] = None,
+        ):
+        
         self._file = file
         if size:
             self._size = size
@@ -46,8 +60,22 @@ class S3ProgressPercentage(object):
         self._seen_so_far = 0
         self._last_percentage = 0
         self._operation_index = operation_index
+        self._manager = manager
 
     def __call__(self, bytes_amount):
+        current_thread = threading.current_thread()
+
+        if isinstance(current_thread, ExitableThread) and current_thread.should_exit and self._manager is not None:
+            # see https://github.com/boto/s3transfer/pull/144
+            # use shutdown when fixed
+            self._manager._shutdown(cancel=True, cancel_msg='Job cancelled')
+            return
+        elif not isinstance(current_thread, ExitableThread):
+            logger.warning('S3ProgressPercentage not running in ExitableThread!!!!!')
+        elif current_thread is threading.main_thread():
+            logger.warning('S3ProgressPercentage __call__ running in main thread!')
+
+
         # To simplify, assume this is hooked up to a single filename
         self._seen_so_far += bytes_amount
         percentage = (self._seen_so_far / self._size) * 100
